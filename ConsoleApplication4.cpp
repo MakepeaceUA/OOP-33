afb2f494e5ac3712928953284a821e906710de23
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-04-13 22:06:57 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstring> 
+using namespace std;
+
+class Fraction 
+{
+private:
+	int m_numerator = 0;
+	int m_denominator = 1;
+public:
+	Fraction(int numerator = 0, int denominator = 1) : m_numerator(numerator), m_denominator(denominator) 
+	{}
+
+	friend ostream& operator<<(ostream&out,const Fraction& f1) 
+	{
+		out << f1.m_numerator << "/" << f1.m_denominator;
+		return out;
+	}
+};
+
+void printFraction(unique_ptr<Fraction>& ptr)
+{
+	if(ptr) 
+	{
+		cout << *ptr;
+	}
+}
+
+int main() 
+{
+	unique_ptr<Fraction> ptr(new Fraction(7, 9));
+	printFraction(ptr);
+}
